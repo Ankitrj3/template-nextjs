7bf6d7b7e4c87bf2e79d99e35b75888418da07db
Vikas <xanderbilla@gmail.com>
2024-09-07 14:16:41 +0530

    SIH_Fixed_02# Updated Forum

--- src/app/(pages)/forum/page.tsx
+++ src/app/(pages)/forum/page.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import Forum from './Components/Forum'
+import Forum from "./components/Forum";
 
-type Props = {}
+type Props = {};
 
 export default function page({}: Props) {
   return (
     <main className="min-h-[calc(100vh-14rem)] flex flex-col items-start justify-start my-8 gap-8">
-    <Forum/>
-  </main>
-  )
+      <Forum />
+    </main>
+  );
 }
